1578c91d2f84c612017f22eeeee65b45fd5dee1e
Leif Andersen <leif@leifandersen.net>
2020-11-23 18:14:38 -0500

    Prune some deps.

--- project.clj
+++ project.clj
@@ -1,10 +1,4 @@
 (defproject interactive-syntax "0.1.0-SNAPSHOT"
-  :description "Enables Interactive-Syntax Extensions in ClojureScript"
-  :url "http://lang.video"
-  :license {:name "Apache 2.0"
-            :url "http://www.apache.org/licenses/LICENSE-2.0"}
-
-
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.773"]
                  ;;[org.clojure/clojurescript "1.10.741"]
